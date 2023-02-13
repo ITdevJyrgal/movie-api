d6fa4cd737215f620c69975293243fd3182013b0
talantDev001 <orpozkanovtalant@gmail.com>
2023-02-13 15:17:23 +0600

    api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import {Routes, Route} from 'react-router-dom'
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Home from "./page/home/home";
+import Popular from "./page/popular/popular";
+import TopRated from "./page/topRated/index";
+import DetailPage from "./page/detailPage";
+import UpComing from "./page/upComing/upComing";
+// setItem(key,value) -
+// getItem(key) -
+// removeItem(key) -
+
+// JSON.parse -
+// JSON.stringify -
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [mode, setMode] = useState(JSON.parse(localStorage.getItem('mode') || false))
+
+    const changeThem = (mode) => {
+        setMode(!mode)
+        localStorage.setItem('mode', JSON.stringify(!mode))
+    }
+
+    return (
+        <div style={{
+            background: mode ? "black" : "",
+            color: mode ? "white" : "",
+            transition:".7s"
+        }}>
+            <Header changeThem={changeThem}/>
+             <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/popular"} element={<Popular/>}/>
+                <Route path={"/top-rated"} element={<TopRated/>}/>
+                <Route path={"/top-rated/rated-info/:id"} element={<DetailPage/>}/>
+                 <Route path={"/up-coming"} element={<UpComing/>} />
+             </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
